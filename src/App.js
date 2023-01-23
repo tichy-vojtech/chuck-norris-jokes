b8b1c2b7de2273c8cc674c6184f461209a57b4e7
Daniel Lesko <dles@ciklum.com>
2023-01-23 09:19:29 +0100

    displaying random image

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-import axios from "axios";
-import JokeCard from './components/JokeCard';
-import { useEffect, useState } from 'react';
-import { Button } from '@chakra-ui/react';
 import { AppLayout } from "./components/AppLayout";
 import { JokesPage } from "./pages/JokesPage";
 import FavoriteJokesPage from "./pages/FavoriteJokesPage";
 // import Sidebar from './components/Sidebar';
 
 function App() {
-
-  const [joke, setJoke] = useState([])
-
-const endpoint = "https://api.chucknorris.io/jokes/random"
-
-useEffect(() => {
-  getJokes()
-}, [])
-
-async function getJokes() {
-	try {
-		const response = await axios.get(endpoint);
-    setJoke(response.data)
-	}
-	catch (error) {
-		console.log(error);
-	}
-}
-
-console.log(joke)
 
   return (
     <div className="App">
