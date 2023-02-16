d8df816ec1a89efbcd0eb86216fa126473883286
tichy-vojtech <113892881+tichy-vojtech@users.noreply.github.com>
2023-02-16 09:17:30 +0100

    Feature/apollo server (#42)
    * moved FE to client folder
    
    * created server folder
    
    * apollo-client
    
    * created schema file
    
    * created resolvers file
    
    * final touches in server folder
    
    * categories fetched on client side
    
    * vercel.json
    
    * server URI changed
    
    ---------
    
    Co-authored-by: Daniel Lesko <dles@ciklum.com>

--- client/src/utils/api/apollo-client.js
+++ client/src/utils/api/apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:5000",
+  uri: "https://cnu-jokes-server.vercel.app/",
   cache: new InMemoryCache(),
 });
 
