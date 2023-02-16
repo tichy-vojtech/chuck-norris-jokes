789dfb406ccd971bd167c7b5b3d655d1c9be1bb0
tichy-vojtech <vojta.t.1999@seznam.cz>
2023-02-16 13:44:02 +0100

    quickfix

--- client/src/api/apollo-client.js
+++ client/src/api/apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: process.env.GRAPH_QL || "http://localhost:5000",
+  uri: process.env.GRAPH_QL,
   cache: new InMemoryCache(),
 });
 
