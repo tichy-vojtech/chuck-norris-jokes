19ada2b8516ec84b05def1d923e2105531ec395a
Daniel Lesko <dles@ciklum.com>
2023-02-16 13:46:35 +0100

    changed uri in apollo-client

--- client/src/api/apollo-client.js
+++ client/src/api/apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: process.env.GRAPH_QL,
+  uri: "https://cnu-jokes-server.vercel.app/",
   cache: new InMemoryCache(),
 });
 
